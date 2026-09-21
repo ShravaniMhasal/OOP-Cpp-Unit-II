6faef0639c3101b074657284f01602b87d563688
ShravaniMhasal <shravanimnextgen@gmail.com>
2026-09-21 12:12:26 +0530

    Add Program 4

--- Program_04/program_04.cpp
+++ Program_04/program_04.cpp
@@ -1,50 +1,68 @@
 #include <iostream>
 #include <string>
 #include <utility>
+using namespace std;
 
+// this is the topmost base class, most general one
+// everyone (Employee, Manager) will eventually get "name" from here
 class Person {
 protected:
-std::string name;
+    // protected so that child classes can use "name" directly
+    // if I made it private, Employee and Manager wouldn't be able to touch it
+    string name;
 
 public:
-explicit Person(std::string personName) : name(std::move(personName)) {}
+    // constructor - runs when a Person object is made, just sets the name
+    explicit Person(string personName) : name(move(personName)) {}
 
-void showPerson() const {
-std::cout << "Name: " << name << '\n';
-}
-
+    // function to print name, any child class can call this
+    void showPerson() const {
+        cout << "Name: " << name << '\n';
+    }
 };
 
+// Employee inherits from Person -> so "an Employee IS A Person" + extra stuff (id)
 class Employee : public Person {
 protected:
-int employeeId;
+    // protected here too, so that Manager (which inherits from Employee) can use it
+    int employeeId;
 
 public:
-Employee(std::string employeeName, int id)
-: Person(std::move(employeeName)), employeeId(id) {}
-void showEmployee() const {
-std::cout << "Employee ID: " << employeeId << '\n';
-}
+    // constructor - first sets up the Person part (name), then sets employeeId
+    Employee(string employeeName, int id)
+        : Person(move(employeeName)), employeeId(id) {}
+
+    // prints employee id
+    void showEmployee() const {
+        cout << "Employee ID: " << employeeId << '\n';
+    }
 };
 
+// Manager inherits from Employee -> so "a Manager IS AN Employee" + extra stuff (teamSize)
+// this makes it a multi-level inheritance chain: Person -> Employee -> Manager
 class Manager : public Employee {
 private:
-int teamSize;
+    // private because only Manager needs this, no other class needs to touch teamSize
+    int teamSize;
 
 public:
-Manager(std::string managerName, int id, int size)
-: Employee(std::move(managerName), id), teamSize(size) {}
+    // constructor - calls Employee's constructor first (which itself calls Person's constructor)
+    // then sets teamSize for Manager
+    Manager(string managerName, int id, int size)
+        : Employee(move(managerName), id), teamSize(size) {}
 
-void showManager() const {
-showPerson();
-showEmployee();
-std::cout << "Team Size: " << teamSize << '\n';
-}
+    // this function shows everything - name from Person, id from Employee, and teamSize from itself
+    void showManager() const {
+        showPerson();   // inherited all the way from Person
+        showEmployee(); // inherited from Employee
+        cout << "Team Size: " << teamSize << '\n'; // Manager's own data
+    }
 };
 
 int main() {
-Manager manager("Ravi", 501, 8);
-manager.showManager();
-return 0;
-
+    // making a Manager triggers the whole chain:
+    // Person's constructor runs first, then Employee's, then Manager's
+    Manager manager("shravani", 987, 8);
+    manager.showManager();
+    return 0;
 }
